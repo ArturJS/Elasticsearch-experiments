e6b1db20fbbdf3790801f3011b9cb71354442849
ArturJS <nizamutdin.art@gmail.com>
2018-07-08 12:35:18 +0400

    Added more test data

--- src/main.js
+++ src/main.js
@@ -1,38 +1,55 @@
 const esClient = require('./es-client');
 
+const random = () => Math.round(Math.random() * 100 + 1);
+
 const bulkCreate = async () => {
-    const students = [
-        {
-            id: 1,
-            first_name: 'Jack',
-            last_name: 'Doe',
+    // const students = [
+    //     {
+    //         id: 1,
+    //         first_name: 'Jack',
+    //         last_name: 'Doe',
+    //         skills: {
+    //             react: 70,
+    //             js: 80,
+    //             vue: 30
+    //         }
+    //     },
+    //     {
+    //         id: 2,
+    //         first_name: 'Bob',
+    //         last_name: 'Burk',
+    //         skills: {
+    //             react: 10,
+    //             js: 20,
+    //             vue: 50
+    //         }
+    //     },
+    //     {
+    //         id: 3,
+    //         first_name: 'Sabrina',
+    //         last_name: 'Galloway',
+    //         skills: {
+    //             react: 50,
+    //             js: 20,
+    //             vue: 100
+    //         }
+    //     }
+    // ];
+    const students = [];
+
+    for (let i = 4; i <= 100; i++) {
+        students.push({
+            id: i,
+            first_name: `Firstname ${i}`,
+            last_name: `Lastname ${i}`,
             skills: {
-                react: 70,
-                js: 80,
-                vue: 30
+                react: random(),
+                js: random(),
+                vue: random()
             }
-        },
-        {
-            id: 2,
-            first_name: 'Bob',
-            last_name: 'Burk',
-            skills: {
-                react: 10,
-                js: 20,
-                vue: 50
-            }
-        },
-        {
-            id: 3,
-            first_name: 'Sabrina',
-            last_name: 'Galloway',
-            skills: {
-                react: 50,
-                js: 20,
-                vue: 100
-            }
-        }
-    ];
+        });
+    }
+
     const bulkCommands = [];
 
     students.forEach(student => {
@@ -66,12 +83,15 @@ const getAll = async ({ query, sort } = {}) => {
         index: 'test_index',
         body: {
             query,
-            sort
+            sort,
+            size: 100
         }
     });
     const students = response.hits.hits;
 
     console.log(JSON.stringify(students, null, '  '));
+
+    console.log('Total count: ', students.length);
 };
 
 // clearIndex();
@@ -86,6 +106,7 @@ getAll({
     //         }
     //     }
     // },
+    //
     sort: {
         _script: {
             type: 'number',
@@ -99,7 +120,7 @@ getAll({
                     return total;
                 `,
                 params: {
-                    search_fields: ['js'] //['react', 'js', 'vue']
+                    search_fields: ['react', 'js', 'vue']
                 }
             },
             order: 'asc'
